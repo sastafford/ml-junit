6acd694ae62e88e4b51ac8e69ec421fce0912a48
Rob Rudin <rjrudin@gmail.com>
2015-02-15 15:08:58 -0500

    Copying and repackaging code from marklogic-java/ml-junit

---
+++ src/main/java/com/marklogic/junit/spring/BasicTestConfig.java
@@ -0,0 +1,40 @@
+package com.marklogic.junit.spring;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.marklogic.clientutil.spring.BasicConfig;
+
+/**
+ * Extends BasicTestConfig with ports for a test application.
+ */
+@Configuration
+@PropertySource({ "file:gradle.properties" })
+public class BasicTestConfig extends BasicConfig {
+
+    @Value("${mlTestRestPort:0}")
+    private Integer mlTestRestPort;
+
+    @Value("${mlTestXdbcPort:0}")
+    private Integer mlTestXdbcPort;
+
+    @Override
+    protected Integer getRestPort() {
+        return (mlTestRestPort != null && mlTestRestPort > 0) ? mlTestRestPort : getMlRestPort();
+    }
+
+    @Override
+    protected Integer getXdbcPort() {
+        return (mlTestXdbcPort != null && mlTestXdbcPort > 0) ? mlTestXdbcPort : getMlXdbcPort();
+    }
+
+    public Integer getMlTestRestPort() {
+        return mlTestRestPort;
+    }
+
+    public Integer getMlTestXdbcPort() {
+        return mlTestXdbcPort;
+    }
+
+}
---
+++ src/main/java/com/marklogic/junit/spring/ModulesLoadedEvent.java
@@ -0,0 +1,19 @@
+package com.marklogic.junit.spring;
+
+import java.io.File;
+import java.util.Set;
+
+import org.springframework.context.ApplicationEvent;
+
+@SuppressWarnings("serial")
+public class ModulesLoadedEvent extends ApplicationEvent {
+
+    public ModulesLoadedEvent(Set<File> loadedModules) {
+        super(loadedModules);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Set<File> getLoadedModules() {
+        return (Set<File>) this.getSource();
+    }
+}
